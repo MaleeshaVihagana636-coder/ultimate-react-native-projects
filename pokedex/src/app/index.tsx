76df95816d4df2526ee9491cdff0035bdbfc8ad4
Maleesha Vihagana <maleeshavihagana636@gmail.com>
2026-08-01 00:55:57 +0530

    feat: implement Pokemon fetching functionality and update greeting message

--- pokedex/src/app/index.tsx
+++ pokedex/src/app/index.tsx
@@ -1,9 +1,25 @@
 import { Text, View, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
 
 export default function Index() {
+  useEffect(() => {
+    //! Fetch Pokemon
+    fetchPokemon();
+  });
+  async function fetchPokemon() {
+    try {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon/?limit=20",
+      );
+      const Data = await response.json();
+      console.log(Data);
+    } catch (erorr) {
+      console.log(erorr);
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text>Hello, world!</Text>
     </View>
   );
 }
